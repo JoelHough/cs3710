374ed4b018e0fdd10b1e26dc51752783b33ce4e6
Joel Hough <joel@joelhough.com>
2013-09-23 04:40:52 -0600

    commented the alu demo

--- alu_demo/alu_onboard_test.v
+++ alu_demo/alu_onboard_test.v
@@ -32,8 +32,10 @@ module alu_onboard_test(
     output Vsync
     );
  
+    /* light the switch leds to show that we see the switches */
     assign Led = sw;
     
+    /* divide the clock. 100MHz in, 25MHz pixel clock and a much slower debounce clock out */
     wire pixel_clk, db_clk;
     reg [15:0] clock_counter = 16'b0;
     assign pixel_clk = clock_counter[1];
@@ -41,15 +43,23 @@ module alu_onboard_test(
     always @(posedge clk)
       clock_counter <= clock_counter + 1'b1;
     
+    /* all the display elements have their output or'd and run to the font rom on this bus.
+       they set their output to 0 when not displaying so they don't or with other chars.
+       if no elements are displaying (~display_char_p) then default to " " */
     wor [7:0] display_char;
     wor display_char_p;
     assign display_char = display_char_p ? 8'b0 : " ";
     
-    wire [2:0] display_char_x;
-    wire [3:0] display_char_y;
-    wire pixel;
-    wire blank;
+    /* screen coords and status */
+    wire [2:0] display_char_x; // aka. column
+    wire [3:0] display_char_y; // aka. row
+    wire pixel; // whether the current pixel is on or not
+    wire blank; // as in screen blank
     
+    /* set the screen. white on, blue off, black during blanking */
+    assign vgaColor = blank ? 8'b0 : (pixel ? 8'b11_111_111 : 8'b10_010_010);
+
+    /* current pixel coords */
     wire [6:0] screen_x;
     wire [4:0] screen_y;
     
@@ -61,6 +71,8 @@ module alu_onboard_test(
                                 .blank(blank),
                                 .x({screen_x, display_char_x}),
                                 .y({screen_y, display_char_y}));
+
+    /* pushbutton wires and debouncing */
     wire up;
     wire down;
     wire left;
@@ -73,13 +85,17 @@ module alu_onboard_test(
     debounce db_right (.clk(db_clk), .bouncy(btnRight), .debounced(right));
     debounce db_center (.clk(db_clk), .bouncy(btnCenter), .debounced(center));
     
+    /* switches encode one byte. buttons load this byte into a and b registers as follows:
+       left  -> a hi
+       up    -> a lo
+       down  -> b hi
+       right -> b lo */
     always @(posedge left) a[15:8] <= sw;
     always @(posedge up) a[7:0] <= sw;
     always @(posedge down) b[15:8] <= sw;
     always @(posedge right) b[7:0] <= sw;
     
-    wire [2:0] op_count;
-    mod_counter #(.MAX(7)) op_counter (.clk(center), .count(op_count), .reset(1'b0), .rollover());
+    /* wire up the alu */
     reg [15:0] a = 16'hCAFE;
     reg [15:0] b = 16'h1234;
     reg [alu.OPCODE_WIDTH-1:0] opcode = 0;
@@ -88,6 +104,11 @@ module alu_onboard_test(
     reg [7:0] opchar = " ";
     alu uut (.a(a), .b(b), .opcode(opcode), .result(result), .flags(flags));
     
+    /* cycle through the ops on a button press */
+    wire [2:0] op_count;
+    mod_counter #(.MAX(7)) op_counter (.clk(center), .count(op_count), .reset(1'b0), .rollover());
+
+    /* set the opcode for the alu and a char to display for it */
     always @(op_count)
       case (op_count)
         0 : begin
@@ -124,6 +145,7 @@ module alu_onboard_test(
         end
       endcase
 
+    /* screen display elements */
     char_string #(.ROW(9), .COL(37), .LENGTH(5), .STRING("CLFZN"))
       flag_labels (.row(screen_y), .col(screen_x), .enable(1'b1),
         .char_p(display_char_p), .char(display_char));
@@ -145,6 +167,7 @@ module alu_onboard_test(
         .char_p(display_char_p), .char(display_char),
         .data(result));
     
+    /* display the opchar next to the hex display and the bin display */
     wire op_char_p;
     assign op_char_p = screen_y == 13 && (screen_x == 15 || screen_x == 48);
     assign display_char =  op_char_p ? opchar : 1'b0;
@@ -162,7 +185,5 @@ module alu_onboard_test(
       result_binary_word (.row(screen_y), .col(screen_x), .enable(1'b1),
         .char_p(display_char_p), .char(display_char),
         .data(result));
-    
-    assign vgaColor = blank ? 8'b0 : (pixel ? 8'b11_111_111 : 8'b10_010_010);
 
 endmodule
